b49875d399cd45fd9f83c2b36c68b3838fffb94f
aswanth-asokan <104835365+aswanth-asokan@users.noreply.github.com>
2022-11-11 18:30:07 +0530

    Delete application_start.sh

--- scripts/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /var/www/micro-service/
-sudo pm2 start auth/index.js --name "auth"
-sudo pm2 start gateway/index.js --name "gateway"
-#sudo pm2 start user/src/index.js --name "user"
-#sudo pm2 start video/src/index.js --name "video"
